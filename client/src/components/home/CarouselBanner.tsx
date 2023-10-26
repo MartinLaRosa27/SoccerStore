63f1175899cdaf127e72aee8ec8eec4c5384c8bb
martinlarosa27 <martinlarosa27@gmail.com>
2023-10-26 16:22:10 -0300

    banner img

--- client/src/components/home/CarouselBanner.tsx
+++ client/src/components/home/CarouselBanner.tsx
@@ -1,17 +1,24 @@
 import Carousel from "react-bootstrap/Carousel";
-import ExampleCarouselImage from "../../assets/img/banners/bannerUno.webp";
+import bannerUno from "../../assets/img/banners/bannerUno.webp";
+import bannerDos from "../../assets/img/banners/bannerDos.webp";
+import bannerTres from "../../assets/img/banners/bannerTres.webp";
 
 function CarouselBanner() {
   const banners = [
     {
       id: 1,
-      img: ExampleCarouselImage,
-      title: "scsc",
+      img: bannerUno,
+      title: "River Plate Away 23/24",
     },
     {
       id: 2,
-      img: ExampleCarouselImage,
-      title: "scsc",
+      img: bannerDos,
+      title: "Boca Juniors Away 23/24",
+    },
+    {
+      id: 3,
+      img: bannerTres,
+      title: "Talleres Away 23/24",
     },
   ];
 
