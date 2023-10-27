66c5d07a8f6c67cf144222d0a6995dfbf72d8f5d
martinlarosa27 <martinlarosa27@gmail.com>
2023-10-27 18:28:23 -0300

    precarrito responsive

--- client/src/components/producto/index.tsx
+++ client/src/components/producto/index.tsx
@@ -6,22 +6,19 @@ function Producto() {
     <>
       <div id="Product">
         <div className="container-prod">
-          <div className="images">
-            <img src="https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw9f2fbc64/products/ADHT3679/ADHT3679-1.JPG" />
+          <div className="info">
+            <div className="images">
+              <img src="https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw9f2fbc64/products/ADHT3679/ADHT3679-1.JPG" />
+            </div>
+            <p className="pick">Selecciona talle:</p>
+            <div className="sizes">
+              <div className="size">S</div>
+              <div className="size">M</div>
+              <div className="size">L</div>
+              <div className="size">XL</div>
+            </div>
           </div>
-          <div className="slideshow-buttons">
-            <div className="one"></div>
-            <div className="two"></div>
-            <div className="three"></div>
-            <div className="four"></div>
-          </div>
-          <p className="pick">Selecciona talle:</p>
-          <div className="sizes">
-            <div className="size">S</div>
-            <div className="size">M</div>
-            <div className="size">L</div>
-            <div className="size">XL</div>
-          </div>
+
           <div className="product">
             <p className="marca">adidas</p>
             <h1>River Plate LOCAL</h1>
@@ -32,7 +29,6 @@ function Producto() {
               only enhances the feeling of moving forward, but makes running
               feel fun, too.
             </p>
-
             <span>Disponibles: {10}</span>
             <div className="buttons mt-2">
               <button className="add">agregar</button>
