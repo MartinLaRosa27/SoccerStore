2cd5bc20d00cfbbb787ac5758072ca9d1b69b5c4
martinlarosa27 <martinlarosa27@gmail.com>
2023-11-02 02:03:28 -0300

    fix tarjetas

--- client/src/components/categoria/index.tsx
+++ client/src/components/categoria/index.tsx
@@ -1,65 +1,38 @@
 import { useEffect, useState } from "react";
 import { useProductoContext } from "../../context/ProductoContext";
-import { formatPrecio } from "../../helpers/formatPrecio";
 import { useParams } from "react-router-dom";
 import Spinner from "../global/Spinner";
 import "./categoria.scss";
+import ProductCard from "../global/ProductCard";
 
 function Categoria() {
   const { getProductoPorCategoria } = useProductoContext();
   const { categoriaId }: any = useParams();
-  const [producto, setProducto] = useState<any[]>([]);
+  const [productos, setProductos] = useState<any[]>([]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    setProducto([]);
+    setProductos([]);
     callGetProductoPorCategoria();
   }, [categoriaId]);
 
   const callGetProductoPorCategoria = async () => {
-    setProducto(await getProductoPorCategoria(Number(categoriaId)));
+    setProductos(await getProductoPorCategoria(Number(categoriaId)));
   };
 
   return (
-    <>
-      {producto.length > 0 && (
+    <div id="Categoria">
+      {productos.length > 0 && (
         <>
-          <div id="Categoria">
-            <div className="container-prod">
-              <div className="info">
-                <div className="images">
-                  <img src={producto[0].urlImg} />
-                </div>
-                <p className="pick">Selecciona talle:</p>
-                <div className="sizes">
-                  {producto[0].talleS ? <div className="size">S</div> : ""}
-                  {producto[0].talleM ? <div className="size">M</div> : ""}
-                  {producto[0].talleL ? <div className="size">L</div> : ""}
-                  {producto[0].talleXL ? <div className="size">XL</div> : ""}
-                </div>
-              </div>
-
-              <div className="product">
-                <p className="marca">{producto[0].marcaNombre}</p>
-                <h1>{producto[0].nombre}</h1>
-                <h2 className="price">${formatPrecio(producto[0].precio)}</h2>
-                <p className="desc">{producto[0].descripcion}</p>
-                <span>
-                  Disponibles: <strong>{producto[0].cantidad}</strong>
-                </span>
-                <div className="buttons mt-2">
-                  <button className="add">agregar</button>
-                  <button className="like">
-                    <span>♥</span>
-                  </button>
-                </div>
-              </div>
-            </div>
+          <div className="row row-cols-2 row-cols-md-5 g-4">
+            {productos.map((producto, i) => {
+              return <ProductCard key={i} producto={producto} />;
+            })}
           </div>
         </>
       )}
-      {producto.length == 0 && <Spinner />}
-    </>
+      {productos.length == 0 && <Spinner />}
+    </div>
   );
 }
 
