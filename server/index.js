0e3bc3155a77ebce0231ea0fb50f1c9ea8bcf965
martinlarosa27 <martinlarosa27@gmail.com>
2023-11-13 19:43:53 -0300

    s

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const { typeDefs } = require("./graph/schema");
 const { resolvers } = require("./graph/resolvers");
 const { DBConnection } = require("./config/DBConnection");
 const { auth } = require("./middleware/auth");
+const Producto = require("./models/Producto");
+const { QueryTypes } = require("sequelize");
 
 DBConnection();
 
@@ -21,3 +23,23 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`The server is running in the URL ${url}`);
 });
+
+
+
+const smallOperation = async () => {
+  try {
+    const productos = await Producto.sequelize.query(
+      `SELECT * FROM productos;`,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
+    console.log(productos);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+setInterval(() => {
+  smallOperation();
+}, 600000);
